d482fdd7f79cf898d638ef782c058f52884e486f
hanxiao <1062708510@qq.com>
2019-04-02 19:32:47 +0800

    shiro

---
+++ Shiro/ShiroWeb/src/main/java/hanxiao/shiro/MyWebEnvironment.java
@@ -0,0 +1,7 @@
+package hanxiao.shiro;
+
+import org.apache.shiro.web.env.DefaultWebEnvironment;
+
+public class MyWebEnvironment extends DefaultWebEnvironment{
+
+}
---
+++ Shiro/ShiroWithJdbc/src/main/java/hanxiao/shiro/User.java
@@ -0,0 +1,27 @@
+package hanxiao.shiro;
+
+public class User {
+	
+	private int id;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	private String name;
+    private String password;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
